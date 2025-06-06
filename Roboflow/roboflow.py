ff1e0d18ff08139dddd8825e70e3781926203994
Jhalaaum <137584056+Jhalaaum@users.noreply.github.com>
2025-06-06 06:22:22 -0700

    x

--- Roboflow/roboflow.py
+++ Roboflow/roboflow.py
@@ -2,7 +2,7 @@ from inference import InferencePipeline
 from inference.core.interfaces.stream.sinks import render_boxes
 
 pipeline = InferencePipeline.init(
-    model_id="rock-paper-scissors-sxsw/11",
+    model_id="waste-classifier-louut/1",
     video_reference=0,
     on_prediction=render_boxes,
 )
